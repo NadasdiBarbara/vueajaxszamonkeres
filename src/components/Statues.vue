5e7aa9d1e38c1563f228c958c7790e41038b379c
Nadasdi Barbara <nadasdibarbara15@gmail.com>
2022-02-08 16:52:38 +0100

    szerkesztés gomb után működik a mentés és a mégse gomb

--- src/components/Statues.vue
+++ src/components/Statues.vue
@@ -32,8 +32,9 @@
             <input type="number" v-model="statue.price">
           </td>
           <td>            
-            <button @click="newStatue">Mentés</button>
-            <button>Mégse</button>
+            <button v-if="mod_new" @click="newStatue" :disabled="saving">Létrehoz</button>
+            <button v-if="!mod_new" @click="saveStatue" :disabled="saving">Mentés</button>
+            <button v-if="!mod_new" @click="cancelEdit" :disabled="saving">Mégse</button>
           </td>
         </tr>
       </tbody>
@@ -85,6 +86,28 @@ export default {
      await this.loadData()
      this.saving=false
      this.resetForm()
+    },
+    async saveStatue() {
+      this.saving='disabled'
+     await fetch(`http://127.0.0.1:8000/api/statues/${this.statue.id}`, {
+       method: 'PATCH',
+       headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       },
+       body: JSON.stringify(this.statue) 
+     })
+     await this.loadData()
+     this.saving=false
+     this.resetForm()
+    },cancelEdit () {
+      this.resetForm()
+    },
+    async editStatue(id) {
+      let Response = await fetch(`http://127.0.0.1:8000/api/statues/${id}`)
+      let data = await Response.json()
+      this.statue = {...data};
+      this.mod_new = false
     },
     resetForm() {
       this.statue = {
